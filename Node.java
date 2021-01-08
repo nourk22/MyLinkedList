4d56b13a6905b6204610f1d6e241827d37c688ea
Nour <>
2021-01-07 22:46:10 -0500

    constructor and get

---
+++ Node.java
@@ -0,0 +1,28 @@
+public class Node{
+
+  //instance variables
+  private String data;
+  private Node next,prev;
+
+  //constructor
+  public Node(String value){
+    data = value;
+    next = null;
+    prev = null;
+  }
+
+  public String getData(){
+    return data;
+  }
+
+  public Node getNext(){
+    return next;
+  }
+
+  public Node getPrev(){
+    return prev;
+  }
+  //write get/set methods for all three instance variables.
+
+
+}
